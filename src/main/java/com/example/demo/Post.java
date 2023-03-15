b1906252f3ff7e4ca84df61d83a41c36aecb6c2d
Rakib Hosen <rakib52112@gmail.com>
2023-03-15 23:51:49 +0600

    add post model

---
+++ src/main/java/com/example/demo/Post.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+public class Post {
+    private Long id;
+    private String title;
+    public Post() {
+
+    }
+    public Post(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/demo/PostController.java
+++ src/main/java/com/example/demo/PostController.java
@@ -4,11 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/posts")
 public class PostController {
+
+    List<Post> postList = new ArrayList<>(
+           List.of(
+                   new Post(1L,"Hello"),
+                   new Post(2L,"Programing")
+           )
+    );
+
     @GetMapping("")
-    String findAll(){
-        return "All posts";
+    List<Post> findAll(){
+        return postList;
     }
 }
